0a2bdec2e81d8de9684a82ed49071df154f69f00
Felix Op de Hipt <felixodh85@googlemail.com>
2026-02-04 09:51:52 +0100

    added code to load data as reactive and provide them for follwoing modules

--- R/app_server.R
+++ R/app_server.R
@@ -6,4 +6,19 @@
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  
+  wl_data <- reactiveValues(
+    data = readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
+  )
+  # wl_data <- readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
+    
+  stations_meta <- reactiveValues(
+    data = stations_meta
+  )
+  
+  mod_station_dashboard_server(
+    "station_dashboard_1",
+    wl_data = wl_data,
+    stations_meta = stations_meta
+  )
 }
