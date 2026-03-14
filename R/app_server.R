9b2e5f56e0b121ddeaef676791e26471dbece3a5
Felix Op de Hipt <felixodh85@googlemail.com>
2026-03-14 13:57:04 +0100

    removed auto load of data by uncomment instead data are updated by actnbutt enabled new module for showing current water level status

--- R/app_server.R
+++ R/app_server.R
@@ -7,19 +7,31 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   
-  wl_data <- reactiveValues(
-    # data = fetch_po_data(initial = F)
-    data = readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
-  )
+  # wl_data <- reactiveValues(
+  #   # data = fetch_po_data(initial = F)
+  #   data = readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
+  # )
   # wl_data <- readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
   # wl_data <- fetch_po_data(initial = F)
   stations_meta <- reactiveValues(
-    data = stations_meta
+    data = get_stations()
   )
   
   mod_station_dashboard_server(
     "station_dashboard_1",
-    wl_data = wl_data,
+    # wl_data = wl_data,
     stations_meta = stations_meta
+  )
+  
+  curr_meas <- reactiveValues(
+    # curr_meas <- fetch_po_curr_meas()
+    data = readRDS(po_cache_dir(folder = "dispPO_data",file = "curr_meas.rds"))
+    # curr_meas <- readRDS(po_cache_dir(folder = "dispPO_data",file = "curr_meas.rds"))
+  )
+
+
+  mod_current_state_server(
+    "current_state_1",
+    curr_meas = curr_meas
   )
 }
