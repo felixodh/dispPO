bdc45b3f5835620be53fabc1dadc5db435a73393
Felix Op de Hipt <felixodh85@googlemail.com>
2026-03-24 13:31:49 +0100

    added for shinyapp.io

--- R/fct_path_manager.R
+++ R/fct_path_manager.R
@@ -27,6 +27,16 @@
 #' @export
 po_cache_dir <- function(folder,file) {
   dir <- tools::R_user_dir("dispPO", "cache")
+  
+  # fallback for shinyapps.io
+  if (!dir.exists(dir)) {
+    base_dir <- file.path(tempdir(), "dispPO_cache")
+  }
+  
+  if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+  
+  
+  
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
   dir <- file.path(dir,folder,file)
   if(!file.exists(dir)){
@@ -35,6 +45,7 @@ po_cache_dir <- function(folder,file) {
   dir
 }
 
+po_cache_dir(folder = "dispPO_data",file = "curr_meas.rds")
 #' Initialize cache files on first installation
 #'
 #' This function ensures that required cache files are available in the user's
@@ -61,7 +72,9 @@ po_cache_dir <- function(folder,file) {
 #' }
 first_installation <- function(){
   dir <- tools::R_user_dir("dispPO/dispPo_data", "cache")
+  
   if (!dir.exists(dir)) dir.create(dir, recursive = TRUE)
+  
   if(!any(list.files(dir) %in% c("curr_meas.rds","lst_query.rds","wl_list.rds"))){
     system.file(package = "dispPO")
     package_path <- system.file("cache", package = "dispPO")
