a9fbb768cea80e3dbc5263868f64243aa4a8bf2a
Felix Op de Hipt <felixodh85@googlemail.com>
2026-03-24 15:26:30 +0100

    changed data loading added monitor module

--- R/app_server.R
+++ R/app_server.R
@@ -7,19 +7,21 @@
 app_server <- function(input, output, session) {
   # Your application server logic
   
-  # wl_data <- reactiveValues(
-  #   # data = fetch_po_data(initial = F)
-  #   data = readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
-  # )
+  wl_data <- reactiveValues(
+    data = readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
+  )
+  
   # wl_data <- readRDS(po_cache_dir(folder = "dispPO_data",file = "wl_list.rds"))
-  # wl_data <- fetch_po_data(initial = F)
+
   stations_meta <- reactiveValues(
     data = get_stations()
   )
+  # stations_meta <- get_stations()
+  
   
   mod_station_dashboard_server(
     "station_dashboard_1",
-    # wl_data = wl_data,
+    wl_data = wl_data,
     stations_meta = stations_meta
   )
   
@@ -34,4 +36,9 @@ app_server <- function(input, output, session) {
     "current_state_1",
     curr_meas = curr_meas
   )
+  
+  mod_wl_monitor_server(
+    "wl_monitor_1",
+    stations_meta = stations_meta,
+    wl_data = wl_data)
 }
